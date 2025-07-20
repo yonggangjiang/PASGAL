ed37723fa188683f6e780f12dcdf0ff3a21bafc4
Xiaojun Dong <akatsuki6725@gmail.com>
2025-07-20 00:03:44 -0700

    Add dinic and some small graphs

---
+++ src/Max-Flow/dinic.h
@@ -0,0 +1,89 @@
+#include <queue>
+
+#include "graph.h"
+#include "parlay/sequence.h"
+
+using namespace std;
+using namespace parlay;
+
+template <class Graph>
+class Dinic {
+  using NodeId = typename Graph::NodeId;
+  using EdgeId = typename Graph::EdgeId;
+  using Edge = typename Graph::Edge;
+  using FlowTy = typename Edge::FlowTy;
+
+  // cannot use const Graph& because e.flow is modified
+  Graph& G;
+  sequence<int> level;
+  // 32 should change to 64 if one vertex has more than 2^32 neighbors
+  sequence<uint32_t> iter;
+
+ public:
+  Dinic() = delete;
+  Dinic(Graph& _G) : G(_G) {
+    level = sequence<int>::uninitialized(G.n);
+    iter = sequence<uint32_t>::uninitialized(G.n);
+  }
+
+  bool bfs(NodeId s, NodeId t) {
+    fill(level.begin(), level.end(), -1);
+    level[s] = 0;
+    queue<NodeId> q;
+    q.push(s);
+    while (!q.empty()) {
+      NodeId v = q.front();
+      q.pop();
+      for (EdgeId i = G.offsets[v]; i < G.offsets[v + 1]; i++) {
+        auto& e = G.edges[i];
+        if (e.flow < e.w && level[e.v] < 0) {
+          level[e.v] = level[v] + 1;
+          q.push(e.v);
+        }
+      }
+    }
+    return level[t] != -1;
+  }
+
+  FlowTy dfs(NodeId u, NodeId t, FlowTy upTo) {
+    if (u == t) {
+      return upTo;
+    }
+    FlowTy flow = 0;
+    for (auto& edge_id = iter[u]; edge_id < G.offsets[u + 1] - G.offsets[u];
+         edge_id++) {
+      auto& e = G.edges[G.offsets[u] + edge_id];
+      if (e.flow < e.w && level[e.v] == level[u] + 1) {
+        FlowTy d = dfs(e.v, t, min(upTo - flow, e.w - e.flow));
+        if (d > 0) {
+          e.flow += d;
+          G.edges[G.offsets[e.v] + e.rev].flow -= d;
+          flow += d;
+          if (flow == upTo) {
+            break;
+          }
+        }
+      }
+    }
+    if (flow == 0) {
+      level[u] = -1;
+    }
+    return flow;
+  }
+
+  FlowTy max_flow(NodeId s, NodeId t) {
+    if (s == t) {
+      return 0;
+    }
+    parlay::parallel_for(0, G.m, [&](EdgeId i) { G.edges[i].flow = 0; });
+    FlowTy flow = 0;
+    while (bfs(s, t)) {
+      fill(iter.begin(), iter.end(), 0);
+      FlowTy f;
+      while ((f = dfs(s, t, numeric_limits<FlowTy>::max())) > 0) {
+        flow += f;
+      }
+    }
+    return flow;
+  }
+};
