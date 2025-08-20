2295c01dd6030eae1f7d8c8a44649f10682adf2e
Xiaojun Dong <akatsuki6725@gmail.com>
2025-08-19 21:19:21 -0700

    Add push relabel

---
+++ src/Max-Flow/push-relabel.cpp
@@ -0,0 +1,210 @@
+#include "push-relabel.h"
+
+#include <queue>
+#include <type_traits>
+
+#include "graph.h"
+
+typedef uint32_t NodeId;
+typedef uint64_t EdgeId;
+#ifdef FLOAT
+typedef float FlowTy;
+#else
+typedef int32_t FlowTy;
+#endif
+
+constexpr int NUM_SRC = 5;
+constexpr int NUM_ROUND = 5;
+constexpr int LOG2_WEIGHT = 6;
+constexpr int WEIGHT_RANGE = 1 << LOG2_WEIGHT;
+
+template <class _FlowTy = int32_t>
+class FlowEdge : public BaseEdge<NodeId> {
+ public:
+  using FlowTy = _FlowTy;
+  FlowTy w;  // capacity
+  EdgeId rev;
+  FlowEdge() = default;
+  FlowEdge(NodeId _v, FlowTy _w) : BaseEdge<NodeId>(_v), w(_w) {}
+  bool operator<(const FlowEdge &rhs) const {
+    return this->v < rhs.v || (this->v == rhs.v && w < rhs.w);
+  }
+  bool operator==(const FlowEdge &rhs) const {
+    return this->v == rhs.v && w == rhs.w;
+  }
+};
+
+template <class Graph>
+Graph generate_reverse_edges(const Graph &G) {
+  using Edge = typename Graph::Edge;
+  parlay::sequence<std::tuple<NodeId, NodeId, EdgeId>> edgelist(G.m * 2);
+  parlay::parallel_for(0, G.n, [&](NodeId u) {
+    parlay::parallel_for(G.offsets[u], G.offsets[u + 1], [&](EdgeId i) {
+      // forward edge
+      std::get<0>(edgelist[i * 2]) = u;
+      std::get<1>(edgelist[i * 2]) = G.edges[i].v;
+      std::get<2>(edgelist[i * 2]) = i * 2;
+      // reverse edge
+      std::get<0>(edgelist[i * 2 + 1]) = G.edges[i].v;
+      std::get<1>(edgelist[i * 2 + 1]) = u;
+      std::get<2>(edgelist[i * 2 + 1]) = i * 2 + 1;
+    });
+  });
+  parlay::sort_inplace(parlay::make_slice(edgelist), [&](auto &a, auto &b) {
+    return std::get<0>(a) < std::get<0>(b) ||
+           (std::get<0>(a) == std::get<0>(b) &&
+            std::get<1>(a) < std::get<1>(b));
+  });
+
+  Graph G_rev;
+  G_rev.n = G.n;
+  G_rev.m = G.m * 2;
+  G_rev.offsets = parlay::sequence<EdgeId>(G.n + 1, G.m * 2);
+  G_rev.edges = parlay::sequence<Edge>(G.m * 2);
+  parlay::parallel_for(0, G.m * 2, [&](EdgeId i) {
+    EdgeId id = std::get<2>(edgelist[i]);
+    G_rev.edges[i].v = std::get<1>(edgelist[i]);
+    G_rev.edges[i].w = (id & 1) ? 0 : G.edges[id / 2].w;
+    if (i == 0 || std::get<0>(edgelist[i]) != std::get<0>(edgelist[i - 1])) {
+      G_rev.offsets[std::get<0>(edgelist[i])] = i;
+    }
+  });
+  parlay::scan_inclusive_inplace(
+      parlay::make_slice(G_rev.offsets.rbegin(), G_rev.offsets.rend()),
+      parlay::minm<EdgeId>());
+  parlay::sequence<tuple<uint32_t, uint32_t>> rev_id(G.m);
+  parlay::parallel_for(0, G_rev.n, [&](NodeId u) {
+    parlay::parallel_for(G_rev.offsets[u], G_rev.offsets[u + 1], [&](EdgeId i) {
+      EdgeId id = std::get<2>(edgelist[i]);
+      if (id & 1) {
+        std::get<1>(rev_id[id / 2]) = i;
+      } else {
+        std::get<0>(rev_id[id / 2]) = i;
+      }
+    });
+  });
+
+  parlay::parallel_for(0, G.m * 2, [&](EdgeId i) {
+    EdgeId id = std::get<2>(edgelist[i]);
+    if (id & 1) {
+      G_rev.edges[i].rev = std::get<0>(rev_id[id / 2]);
+    } else {
+      G_rev.edges[i].rev = std::get<1>(rev_id[id / 2]);
+    }
+  });
+
+  // validate
+  parlay::parallel_for(0, G_rev.n, [&](NodeId u) {
+    parlay::parallel_for(G_rev.offsets[u], G_rev.offsets[u + 1], [&](EdgeId i) {
+      FlowTy w = G_rev.edges[i].w;
+      EdgeId rev = G_rev.edges[i].rev;
+      assert(G_rev.edges[rev].v == u);
+      if (w) {
+        assert(G_rev.edges[rev].w == 0);
+      }
+    });
+  });
+  return G_rev;
+}
+template <class Algo, class Graph, class NodeId = typename Graph::NodeId>
+void run(Algo &algo, [[maybe_unused]] Graph &G, NodeId s, NodeId t) {
+  cout << "source " << s << ", target " << t << endl;
+  double total_time = 0;
+  FlowTy max_flow = 0;
+  parlay::sequence<FlowTy> flows = parlay::tabulate(G.m, [&](EdgeId i) {
+    return G.edges[i].w;
+  });
+  for (int i = 0; i <= NUM_ROUND; i++) {
+    parlay::parallel_for(0, G.m, [&](EdgeId j) {
+      G.edges[j].w = flows[j];
+    });
+    internal::timer tm;
+    max_flow = algo.max_flow(s, t);
+    tm.stop();
+    if (i == 0) {
+      cout << "Warmup Round: " << tm.total_time() << endl;
+    } else {
+      cout << "Round " << i << ": " << tm.total_time() << endl;
+      total_time += tm.total_time();
+    }
+    cout << "Max flow: " << max_flow << endl;
+  }
+  double average_time = total_time / NUM_ROUND;
+  cout << "Average time: " << average_time << endl;
+
+  ofstream ofs("push-relabel.tsv", ios_base::app);
+  ofs << s << '\t' << t << '\t' << max_flow << '\t' << average_time << '\n';
+  ofs.close();
+}
+
+template <class Algo, class Graph>
+void run(Algo &algo, Graph &G) {
+  using NodeId = typename Graph::NodeId;
+  for (int v = 0; v < NUM_SRC; v++) {
+    NodeId s = hash32(v) % G.n;
+    NodeId t = hash32(v + NUM_SRC) % G.n;
+    run(algo, G, s, t);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    cout << "Usage: " << argv[0] << " [-i input_file] [-s]\n"
+         << "Options:\n"
+         << "\t-i,\tinput file path\n"
+         << "\t-s,\tsymmetrized input graph\n";
+    return 0;
+  }
+  char c;
+  char const *input_path = nullptr;
+  bool symmetrized = false;
+  uint32_t source = UINT_MAX;
+  uint32_t target = UINT_MAX;
+  while ((c = getopt(argc, argv, "i:sr:t:")) != -1) {
+    switch (c) {
+      case 'i':
+        input_path = optarg;
+        break;
+      case 's':
+        symmetrized = true;
+        break;
+      case 'r':
+        source = atol(optarg);
+        break;
+      case 't':
+        target = atol(optarg);
+        break;
+      default:
+        std::cerr << "Error: Unknown option " << optopt << std::endl;
+        abort();
+    }
+  }
+
+  cout << "Reading graph..." << endl;
+  Graph<NodeId, EdgeId, FlowEdge<FlowTy>> G;
+  G.read_graph(input_path);
+  G.symmetrized = symmetrized;
+  if (!G.weighted) {
+    cout << "Generating edge weights..." << endl;
+    G.generate_random_weight(1, WEIGHT_RANGE);
+  }
+  // for (NodeId i = 0; i < G.n; i++) {
+  //   for (EdgeId j = G.offsets[i]; j < G.offsets[i + 1]; j++) {
+  //     printf("(%u,%u,%d)\n", i, G.edges[j].v, G.edges[j].w);
+  //   }
+  // }
+  // printf("\n");
+
+  G = generate_reverse_edges(G);
+
+  cout << "Running on " << input_path << ": |V|=" << G.n << ", |E|=" << G.m
+       << ", num_src=" << NUM_SRC << ", num_round=" << NUM_ROUND << endl;
+
+  PushRelabel solver(G);
+  if (source == UINT_MAX || target == UINT_MAX) {
+    run(solver, G);
+  } else {
+    run(solver, G, source, target);
+  }
+  return 0;
+}
---
+++ src/Max-Flow/push-relabel.h
@@ -0,0 +1,136 @@
+#include <queue>
+
+#include "graph.h"
+#include "hashbag.h"
+#include "parlay/delayed_sequence.h"
+#include "parlay/sequence.h"
+
+using namespace std;
+using namespace parlay;
+
+template <class Graph>
+class PushRelabel {
+  using NodeId = typename Graph::NodeId;
+  using EdgeId = typename Graph::EdgeId;
+  using Edge = typename Graph::Edge;
+  using FlowTy = typename Edge::FlowTy;
+
+  // cannot use const Graph& because e.w is modified
+  size_t n;
+  NodeId source, sink;
+  Graph& G;
+  hashbag<NodeId> bag;
+  sequence<bool> in_frontier;
+  sequence<NodeId> frontier;
+  sequence<int> heights;
+  sequence<FlowTy> excess;
+
+ public:
+  PushRelabel() = delete;
+  PushRelabel(Graph& _G) : G(_G), bag(G.n) {
+    n = G.n;
+    in_frontier = sequence<bool>(G.n);
+    frontier = sequence<NodeId>::uninitialized(G.n);
+    heights = sequence<int>::uninitialized(G.n);
+    excess = sequence<FlowTy>::uninitialized(G.n);
+  }
+
+  void add_to_bag(NodeId u) {
+    if (u != source && u != sink &&
+        compare_and_swap(&in_frontier[u], false, true)) {
+      bag.insert(u);
+    }
+  }
+
+  void init(NodeId s) {
+    parallel_for(0, G.n, [&](size_t i) {
+      heights[i] = 0;
+      excess[i] = 0;
+    });
+    heights[s] = n;
+    parallel_for(G.offsets[s], G.offsets[s + 1], [&](size_t i) {
+      auto& e = G.edges[i];
+      if (e.w) {
+        G.edges[e.rev].w += e.w;
+        // Assume no duplicate edges. Otherwise it needs atomic addition
+        excess[e.v] += e.w;
+        e.w = 0;
+        add_to_bag(e.v);
+      }
+    });
+  }
+
+  void relabel(NodeId u) {
+    heights[u] = std::numeric_limits<int>::max();
+    for (size_t i = G.offsets[u]; i < G.offsets[u + 1]; i++) {
+      auto& e = G.edges[i];
+      if (e.w > 0) {
+        heights[u] = min(heights[u], heights[e.v]);
+      }
+    }
+    heights[u]++;
+    // printf("node %u relabeled to %d\n", u, heights[u]);
+  }
+
+  void push(NodeId u, Edge& fwd_e, Edge& rev_e) {
+    NodeId v = fwd_e.v;
+    if (heights[u] != heights[v] + 1) {
+      return;
+    }
+    FlowTy excess_u = excess[u];
+    FlowTy send = min(fwd_e.w, excess_u);
+    if (send) {
+      write_add(&excess[u], -send);
+      assert(excess[u] >= 0);
+      write_add(&excess[v], send);
+      fwd_e.w -= send;
+      rev_e.w += send;
+      add_to_bag(v);
+    }
+  }
+
+  void discharge(NodeId u) {
+    bool pushed = false;
+    for (size_t i = G.offsets[u]; i < G.offsets[u + 1]; i++) {
+      auto& e = G.edges[i];
+      if (e.w > 0 && heights[u] == heights[e.v] + 1) {
+        pushed = true;
+        push(u, e, G.edges[e.rev]);
+        if (!excess[u]) {
+          break;
+        }
+      }
+    }
+    if (!pushed) {
+      // printf("relabel %d\n", u);
+      relabel(u);
+      add_to_bag(u);
+    }
+    if (excess[u] > 0) {
+      // printf("node %d has excess %d remaining\n", u, excess[u]);
+      add_to_bag(u);
+    }
+  }
+
+  FlowTy max_flow(NodeId s, NodeId t) {
+    source = s;
+    sink = t;
+    if (source == sink) {
+      return 0;
+    }
+    init(source);
+    while (true) {
+      size_t frontier_size = bag.pack_into(frontier);
+      if (frontier_size == 0) {
+        break;
+      }
+      parallel_for(0, frontier_size,
+                   [&](size_t i) { in_frontier[frontier[i]] = false; });
+      parallel_for(0, frontier_size, [&](size_t i) {
+        NodeId u = frontier[i];
+        discharge(u);
+      });
+    }
+    return excess[sink];
+  }
+};
